11cf095c1c2a242e353275a9dc734c67a9371fb7
Tan <46892603+PanGDX@users.noreply.github.com>
2023-11-23 11:47:51 +0800

    Update autogpt.py

--- autogpt.py
+++ autogpt.py
@@ -15,8 +15,7 @@ def pyautoguifunc(content, number_of_times, counter, timing_interval):
 		time.sleep(timing_interval)
 	return counter+number_of_times
 
-def content_return(files, content_chunk):
-	splitter = "NEWCHAPTERNEWCHAPTERNEWCHAPTERNEWCHAPTER"
+def content_return(files, content_chunk, splitter):
 
 # Splitting the text by the splitter and removing any leading or trailing newlines from each part
 	content = [part.strip() for part in files.read().split(splitter) if part.strip()]
@@ -38,16 +37,17 @@ def content_return(files, content_chunk):
 timing = int(input("Timing interval: "))
 fileinputs = str(input("Input file:"))
 files = open(f"{fileinputs}.txt", "r", encoding = "utf-8")
-content_chunk = str(input("Current Content chunk:"))
-
+content_chunk = str(input("A part of the current prompt to be submitted\nIf starting from the first prompt, press enter\n:"))
+splitter = str(input("Splitter text:"))
 print("Move to ChatGPT. Make sure to click on the form field.")
-content_, counter_ = content_return(files, content_chunk)
+content_, counter_ = content_return(files, content_chunk, splitter)
 
 
 
 time.sleep(5)
 
 while True:
-	counter_ = pyautoguifunc(content_, 5, counter_, timing)#inclusive o the counter
+	print("Note that the program is built for ChatGPT-4. It will run for 30 times and then pause for 3 hours. Remove this if you want it to work for ChatGPT-3 more efficiently")
+	counter_ = pyautoguifunc(content_, 30, counter_, timing)
 	time.sleep(60*60*3)
 	
